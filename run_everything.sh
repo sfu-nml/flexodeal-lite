218e606114517cc7f46c2dfc94312df1f33ec48a
Javier Almonacid <almonacid.javier@gmail.com>
2024-02-23 13:24:19 -0800

    Added parameter files for different paper 1 experiments

---
+++ run_everything.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+./dynamic-muscle q-pennation-00.prm
+./dynamic-muscle q-pennation-05.prm
+./dynamic-muscle q-pennation-10.prm
+./dynamic-muscle q-pennation-15.prm
+./dynamic-muscle q-pennation-20.prm
+./dynamic-muscle q-pennation-25.prm
+./dynamic-muscle q-pennation-30.prm
+./dynamic-muscle q-length-M2.prm
+./dynamic-muscle q-length-M1.prm
+./dynamic-muscle q-length-P1.prm
+./dynamic-muscle q-length-P2.prm
+./dynamic-muscle q-length-P3.prm
+./dynamic-muscle d-pennation-00.prm
+./dynamic-muscle d-pennation-05.prm
+./dynamic-muscle d-pennation-10.prm
+./dynamic-muscle d-pennation-15.prm
+./dynamic-muscle d-pennation-20.prm
+./dynamic-muscle d-pennation-25.prm
+./dynamic-muscle d-pennation-30.prm
+./dynamic-muscle d-length-M2.prm
+./dynamic-muscle d-length-M1.prm
+./dynamic-muscle d-length-P1.prm
+./dynamic-muscle d-length-P2.prm
+./dynamic-muscle d-length-P3.prm
