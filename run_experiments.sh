abf43027c8844e6f9b8264a78a97a01824734020
Javier Almonacid <almonacid.javier@gmail.com>
2024-04-11 16:15:04 -0700

    Added parameters files for batch script

---
+++ run_experiments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./dynamic-muscle parameters_1.prm control_points_strain.dat control_points_activation.dat
+./dynamic-muscle parameters_2.prm control_points_strain.dat control_points_activation.dat
+./dynamic-muscle parameters_3.prm control_points_strain.dat control_points_activation.dat
+./dynamic-muscle parameters_4.prm control_points_strain.dat control_points_activation.dat
+./dynamic-muscle parameters_5.prm control_points_strain.dat control_points_activation.dat
+./dynamic-muscle parameters_6.prm control_points_strain.dat control_points_activation.dat
